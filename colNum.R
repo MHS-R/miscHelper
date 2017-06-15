16cffcd7aa96e5e97db5b2a81b1468a74fe4f65e
Matthew Sigal <matthewsigal@gmail.com>
2017-06-15 11:31:04 -0400

    Small update
    Got a notification from github about these uploads and noticed that this function wasn't quite functional. Here is my suggested change :)

--- colNum.R
+++ colNum.R
@@ -5,3 +5,23 @@ colNum <- function(df){
   colnames(y) <- colnames(x)
   rownames(y) <- "col.number"; return(y)
 } 
+
+#' colNum2 (fixed)
+#' View the column numbers are associated with the data frame column variable names
+#' @param df data.frame
+#' @param wide A boolean indicating if results should be stored in wide or long format
+colNum2 <- function(df, wide = FALSE){
+  if (wide == FALSE) {
+    ret <- data.frame(variable = colnames(df), 
+                      col.number = seq(1,ncol(df)))
+  } else {
+    ret <- t(data.frame(col.number = seq(1, ncol(df))))
+    colnames(ret) <- colnames(df)
+  } 
+  return(ret)
+}
+
+# EXAMPLE  
+data(cars)
+colNum2(cars)
+colNum2(cars, wide = TRUE)
